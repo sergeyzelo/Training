17d85a9d87cc98fb6c0781914b4d3e6e432287ef
Sergey <sergeyzelo@gmail.com>
2020-01-07 13:15:19 +0300

    Домашнее задание

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
 "use strict";
+
+ let money = prompt("Ваш бюджет на месяц?", "");
+let time = prompt("Введите дату в формате YYYY-MM-DD", "");
+
+let appData = {
+    butget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false
+};
+
+for (let i = 0; i < 2; i++) {
+    let a = prompt("Введите обязательную статью расходов в этом месяц", ""),
+        b = prompt("Во сколько обойдется?", "");
+        
+    if ( (typeof(a))==="string" && (typeof(a)) != null && (typeof(b)) != null && a != "" && b != "" && a.length < 50) {
+        console.log("done");
+        appData.expenses[a] = b;
+    } else {
+
+    }
+};
+
+appData.moneyPerDay = appData.butget / 30;
+
+alert("Ежедневный бюджет: " + appData.moneyPerDay);
+
+if(appData.moneyPerDay < 100) {
+    console.log("Минимальный уровень достатка");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 200) {
+    console.log("Средний уровень достатка");
+} else if (appData.moneyPerDay > 2000) {
+    console.log("Выслкий уровень достатка");
+} else {
+    console.log("Произашла ошибка");
+}
+
+
+
